bd0a22590872a3e71eea96c9a1a268b6fadd8df2
vathsala333 <vathsala455@gmail.com>
2026-02-09 22:23:44 +0530

    Added netlify link

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smartdeskserver.onrender.com/api",
 });
 
 
